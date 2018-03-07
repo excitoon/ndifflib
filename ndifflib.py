bf4ab4822269c5f865a6986ccb4f678d343d6d7e
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2018-03-07 14:00:45 +0300

    initial

---
+++ ndifflib.py
@@ -0,0 +1,48 @@
+﻿import difflib
+
+
+class SequenceMatcher(object):
+    def __init__(self, reserved, *args):
+        assert reserved is None
+        self.__combined = []
+        self.__lines = []
+        self.__size = 0
+        for index, i in enumerate(args):
+            self._merge(i, index)
+
+    def _copy_set_list(self, set_list):
+        return list(map(set, set_list))
+
+    def _merge(self, l, tag):
+        diff = difflib.SequenceMatcher(None, self.__combined, l)
+        new_combined = []
+        new_lines = []
+        for action, lhs_begin, lhs_end, rhs_begin, rhs_end in diff.get_opcodes():
+            new_combined.extend(self.__combined[lhs_begin:lhs_end])
+            new_lines.extend(self._copy_set_list(self.__lines[lhs_begin:lhs_end]))
+            if action == 'equal':
+                for i in range(0, rhs_end-rhs_begin):
+                    new_lines[-1-i].add(tag)
+            else:
+                new_combined.extend(l[rhs_begin:rhs_end])
+                new_lines.extend([set([tag])] * (rhs_end - rhs_begin))
+        self.__combined = new_combined
+        self.__lines = new_lines
+        self.__size += 1
+
+    def get_opcodes(self):
+        opcodes = []
+        indices = []
+        for i in range(0, self.__size):
+            indices.append(0)
+        for line in self.__lines:
+            if len(line) == self.__size:
+                action = 'equal'
+            else:
+                action = 'complicated'
+            begins = list(indices)
+            for i in range(0, self.__size):
+                if i in line:
+                    indices[i] += 1
+            opcodes.append((action, begins, list(indices)))
+        return opcodes
---
+++ test_ndifflib.py
@@ -0,0 +1,16 @@
+﻿import unittest
+
+import ndifflib
+
+
+class test_ndifflib(unittest.TestCase):
+    def testSimple(self):
+        lists = [[1, 2, 3], [2, 3, 4], [1, 2, 3, 4], [2, 4], [2, 3]]
+        diff = ndifflib.SequenceMatcher(None, *lists)
+        self.assertEqual(diff.get_opcodes(),
+            [
+                ('complicated', [0, 0, 0, 0, 0], [1, 0, 1, 0, 0]),
+                ('equal',       [1, 0, 1, 0, 0], [2, 1, 2, 1, 1]),
+                ('complicated', [2, 1, 2, 1, 1], [3, 2, 3, 1, 2]),
+                ('complicated', [3, 2, 3, 1, 2], [3, 3, 4, 2, 2])
+            ])
